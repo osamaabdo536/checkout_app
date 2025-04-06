83ee10e3293769b8b734a3a1ad95555fa79aab19
osamaabdo536 <93677924+osamaabdo536@users.noreply.github.com>
2025-04-06 14:45:53 +0200

    initial project.

---
+++ lib/Features/checkout/presentation/views/my_cart_view.dart
@@ -0,0 +1,11 @@
+import 'package:checkout_app/Core/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class MyCartView extends StatelessWidget {
+  const MyCartView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:checkout_app/Features/checkout/presentation/views/my_cart_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CheckoutApp());
+}
+
+class CheckoutApp extends StatelessWidget {
+  const CheckoutApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyCartView(),
+    );
+  }
+}
